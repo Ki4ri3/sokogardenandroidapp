599bd01258af972dc39c8c6fa74f8ac7b2666221
Ki4ri3 <lewiskiarie9@gmail.com>
2026-04-15 14:11:50 +0300

    soko

--- app/src/main/java/com/example/sokogarden/MainActivity.kt
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.sokogarden
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +36,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, Signin::class.java)
             startActivity(intent)
         }
+
+//        find the recyclerview and the progress bar by use of their ids
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressbar)
+
+//        specify the API URL endpoint for fetching the products from always data
+        val url = "https://keyarie.alwaysdata.net/api/get_products"
+
+//        import the helper class
+        val helper = ApiHelper(applicationContext)
+
+//        Inside of the helper class, access the function load products
+        helper.loadProducts(url, recyclerView, progressBar)
 
 
     }
